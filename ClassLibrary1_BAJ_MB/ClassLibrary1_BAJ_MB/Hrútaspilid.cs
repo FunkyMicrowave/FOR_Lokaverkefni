a5735d2cc782604ae47163a0ad530569fab424b3
Spreyazz <brynjolfurjo322@nemi.tskoli.is>
2017-04-06 10:02:02 +0000

    Add files via upload
    Byrjaður á þessu ætla ekki að hafa stoðhesta ætla að finna annað top trumps til að vera með

---
+++ ClassLibrary1_BAJ_MB/ClassLibrary1_BAJ_MB/Hrútaspilid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Brynjolfur Axel Jóhannesson
+ * Matias
+ */
+namespace ClassLibrary1_BAJ_MB
+{
+    class Hrútaspilid : Spil
+    {
+        public string NafnHruts { get; set; }
+        public double ÞyngdHruts { get; set; }
+        public double Mjólkurlagni_Dætra { get; set; }
+        public double Einkunn_Ullar { get; set; }
+        public double Fjöldi_Afkvæma { get; set; }
+        public double Einkunn_Læris { get; set; }
+        public double Frjósemi { get; set; }
+        public double Þykkt_Bakvoðva { get; set; }
+        public double Einkun_fyrir_malir { get; set; }
+        public string Fadir { get; set; }
+        public string Modir { get; set; }
+        public string Nyr_Fallin { get; set; }
+
+}
+}
