99a77be8d127b6a6f3c985b0ebaea92146f22d3a
Spreyazz <brynjolfurjo322@nemi.tskoli.is>
2017-04-26 21:16:45 +0000

    bunnir að bæta öðrum top trumos

---
+++ ClassLibrary1_BAJ_MB/ClassLibrary1_BAJ_MB/ClassLibrary1_BAJ_MB/Risaelduspilid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1_BAJ_MB
+{
+    public class Risaelduspilid : Spil
+    {
+        public string Nafn { get; set; }
+        public double Hæð { get; set; }
+        public string Þyngd { get; set; }
+        public string Lengd { get; set; }
+        public int Drápseðli { get; set; }
+        public string Greind { get; set; }
+        public int Aldur { get; set; } 
+    }
+}
---
+++ ClassLibrary1_BAJ_MB/ClassLibrary1_BAJ_MB/ClassLibrary1_BAJ_MB/Spil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Brynjólfur Axel Jóhannesson
+ * matias
+ */
+namespace ClassLibrary1_BAJ_MB
+{
+    public class Spil
+    {
+        public string Nafn { get; set; }
+        public string Flokkur1 { get; set; }
+        public string Flokkur2 { get; set; }
+        public string Flokkur3 { get; set; }
+        public string Flokkur4 { get; set; }
+        public string Flokkur5 { get; set; }
+        public string Flokkur6 { get; set; }
+        public string Flokkur7 { get; set; }
+        public string Flokkur8 { get; set; }
+        public string Flokkur9 { get; set; }
+        public string Flokkur10 { get; set; }
+        public string Flokkur11 { get; set; }
+    }
+}
