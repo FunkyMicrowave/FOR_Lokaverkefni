c8629340bf6d9b072fd583aabb47476e473413a3
Spreyazz <brynjolfurjo322@nemi.tskoli.is>
2017-04-27 21:03:36 +0000

    Add files via upload
    sjavardyr komið

--- ClassLibrary1_BAJ_MB/ClassLibrary1_BAJ_MB/Sjávardýr.cs
+++ ClassLibrary1_BAJ_MB/ClassLibrary1_BAJ_MB/Sjávardýr.cs
@@ -3,36 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Brynjolfur
+ * Matthias
+ */
 namespace ClassLibrary1_BAJ_MB
 {
     public class Sjávardýr : Spil
     {
-        string NafnSjavardyri;
-        public string Fágæti { get; set; }
-        public string Sædýrasofn { get; set; }
-        public string HættaÁÁfollum { get; set; }
-        public string Verndarþörf { get; set; }
-        public string HættaÁÚtrýmingu { get; set; }
-        string tómt = null;
-
         public Sjávardýr(string nafn, string flokkur1, string flokkur2, string flokkur3, string flokkur4, string flokkur5, string flokkur6)
             : base(nafn, flokkur1, flokkur2, flokkur3, flokkur4, flokkur5, flokkur6)
         {
-            NafnSjavardyri = nafn;
-            flokkur1 = Fágæti;
-            flokkur2 = Sædýrasofn;
-            flokkur3 = HættaÁÁfollum;
-            flokkur4 = Verndarþörf;
-            HættaÁÚtrýmingu = flokkur5;
-            tómt = flokkur6;
+            Nafn = nafn;
+            Flokkur1 = flokkur1;
+            Flokkur2 = flokkur2;
+            Flokkur3 = flokkur3;
+            Flokkur4 = flokkur4;
+            Flokkur5 = flokkur5;
+            Flokkur6 = flokkur6;
         }
 
-          /*   public CD(string comment, string gotIt, string playingTime, string title, string artist, string numberOfTracks)
-            : base(comment, gotIt, playingTime, title)
+        public override string ToString()
         {
-            Artist = artist;
-            NumerOfTracks = numberOfTracks;
-        }//end public CD*/
+            return string.Format("Nafn: {0} Fágæti: {1} Sædýra stofn: {2} Hætta á áfalli: {3} Verndar þörf: {4} Hætta á útrýmingar hættu {5}");
+        }
     }
 }
